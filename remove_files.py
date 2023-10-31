dd28dbb50bc288beebca4e220fb3d87bd0d1401a
unknown <jackcoetzer23@gmail.com>
2023-10-31 19:42:45 +0200

    Show keys

--- remove_files.py
+++ remove_files.py
@@ -5,3 +5,8 @@ for i in range(0,38):
     file = 'auto_'+str(i)+'.pkl'
     if os.path.exists(file): 
         os.remove(file)
+
+for i in range(0,38): 
+    file = '2auto_'+str(i)+'.pkl'
+    if os.path.exists(file): 
+        os.remove(file)
