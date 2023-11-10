76575bb890724069af7281077d43044469c64fb5
unknown <jackcoetzer23@gmail.com>
2023-11-10 10:46:49 +0200

    Naming conv right

--- onelayer.py
+++ onelayer.py
@@ -27,8 +27,8 @@ def normalize(data):
         
 def getFile(_start): 
     for i in range(_start,100):
-        file = "onelayer_relusig_15_10_0.0001_11"+str(i)+".pkl"
-        model_file = "onelayer_relusig_15_10_0.0001_1"+str(i)+".json"
+        file = "onelayer_relusig_1024_15_10_0.0001_11"+str(i)+".pkl"
+        model_file = "onelayer_relusig_1024_15_10_0.0001_1"+str(i)+".json"
         if not os.path.exists(file): 
             Path(file).touch()
             return file, model_file
@@ -86,10 +86,10 @@ latent_dim = 256
 
 # Encoder
 encoder_input = keras.Input(shape=(input_dim,))
-encoder_output = layers.Dense(4096, activation='relu')(encoder_input)
+encoder_output = layers.Dense(1024, activation='relu')(encoder_input)
 
 # Decoder
-decoder_input = keras.Input(shape=(4096,))
+decoder_input = keras.Input(shape=(1024,))
 decoder_output = layers.Dense(input_dim, activation='sigmoid')(decoder_input)
 
 # Models
